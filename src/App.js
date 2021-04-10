68a5ea135a2c1cb38ea985515df5c86917e26412
Zoheb Alli Khan <72716462+ZohebMOPO@users.noreply.github.com>
2021-04-10 10:56:23 +0530

    Updated the App function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import ReactMapGL from 'react-map-gl';
+import mapboxgl from 'mapbox-gl';
 
 
-function App() {
-  const [viewport, setViewport] = useState({
-    width: '100vw',
-    height: '100vh',
-    latitude: 15,
-    longitude: 0,
-    zoom: 1.5
-  });
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+const mapContainerRef = useRef(null);
+
+const App = () => {
+  const mapContainerRef = useRef(null);
+  const popupRef = useRef(new mapboxgl.Popup({ offset:15 }));
+  // initialize map when component mounts
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      // See style options here: https://docs.mapbox.com/api/maps/#styles
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-104.9876, 39.7405],
+      zoom: 12.5,
+    });
+
+    // add navigation control (the +/- zoom buttons)
+    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+
+    // clean up on unmount
+    return () => map.remove();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return <div className="map-container" ref={mapContainerRef} />;
+};
 
   return (
     <ReactMapGL
