e82e5ffd2571bb4a28e7d05692b7fc6a201ec66e
Viet Bui <buiminhviet.2002@gmail.com>
2021-04-10 16:39:28 -0500

    Put events on the map

--- src/App.js
+++ src/App.js
@@ -1,33 +1,65 @@
 import './App.css';
 import { useState, useRef } from 'react';
-import ReactMapGL from 'react-map-gl';
-import mapboxgl from 'mapbox-gl';
+import ReactMapGL, {Marker} from 'react-map-gl';
+import * as eventsData from './data/70s-war-events.json';
+// import mapboxgl from 'mapbox-gl';
 
 
-mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
-const mapContainerRef = useRef(null);
+// mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+// const mapContainerRef = useRef(null);
 
-const App = () => {
-  const mapContainerRef = useRef(null);
-  const popupRef = useRef(new mapboxgl.Popup({ offset:15 }));
-  // initialize map when component mounts
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      // See style options here: https://docs.mapbox.com/api/maps/#styles
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-104.9876, 39.7405],
-      zoom: 12.5,
-    });
+// const App = () => {
+//   const mapContainerRef = useRef(null);
+//   const popupRef = useRef(new mapboxgl.Popup({ offset:15 }));
+//   // initialize map when component mounts
+//   useEffect(() => {
+//     const map = new mapboxgl.Map({
+//       container: mapContainerRef.current,
+//       // See style options here: https://docs.mapbox.com/api/maps/#styles
+//       style: 'mapbox://styles/mapbox/streets-v11',
+//       center: [-104.9876, 39.7405],
+//       zoom: 12.5,
+//     });
 
-    // add navigation control (the +/- zoom buttons)
-    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+//     // add navigation control (the +/- zoom buttons)
+//     map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
 
-    // clean up on unmount
-    return () => map.remove();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+//     // clean up on unmount
+//     return () => map.remove();
+//   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  return <div className="map-container" ref={mapContainerRef} />;
-};
+//   return <div className="map-container" ref={mapContainerRef} />;
+// };
+
+// Comment out above because the code is not working -Try to fix it Zoheb
+function App() {
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 15,
+    longitude: 0,
+    zoom: 1.5
+  });
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      onViewportChange={viewport => {setViewport(viewport)}}
+      mapStyle="mapbox://styles/vietbuiminh/cknb5qncl12ap17lp7dfwujyo"
+    >
+      {eventsData.years.map((event) => (
+        // Marker getting data from JSON file in data folder
+        <Marker 
+          key={event.id} 
+          longitude={event.coordinates[1]}
+          latitude={event.coordinates[0]}
+        >
+          <div>EVENT</div>
+        </Marker>
+      ))}
+    </ReactMapGL>
+  );
+}
 
 export default App;
