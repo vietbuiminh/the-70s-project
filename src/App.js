f47bae758546bb78b1993343b2955234b656d34c
Viet Bui <buiminhviet.2002@gmail.com>
2021-04-09 21:16:34 -0500

    Added React MapBox GL

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactMapGL from "react-map-gl";
+import React, { useState } from "react";
+
 
 function App() {
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100nh',
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          
+        </Navbar.Brand>
+      </Navbar>
+     
+      <ReactMapGL {...viewport}>
+      </ReactMapGL>
     </div>
   );
 }
